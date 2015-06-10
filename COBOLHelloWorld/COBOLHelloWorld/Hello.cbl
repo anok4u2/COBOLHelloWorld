840e28a2d670b11563f008f5b088833905aa132d
anok4u2 <david_sands@btinternet.com>
2015-06-10 22:32:01 +0100

    V1 of COBOL Hello World
    This is a Test Project for test of github.

---
+++ COBOLHelloWorld/COBOLHelloWorld/Hello.cbl
@@ -0,0 +1,16 @@
+      *identification division.
+      *program-id. Hello.
+      *
+      *environment division.
+      *configuration section.
+      *
+      *data division.
+      *working-storage section.
+      *
+      *procedure division.
+       
+           display "Hello COBOL World !"
+
+      *    goback.
+      *
+      *end program Hello.
